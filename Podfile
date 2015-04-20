454fe8f4667082d0e3686c757bb9006adc70736e
Paul Taykalo <tt.kilew@gmail.com>
2015-04-20 15:54:07 +0300

    Base project structure update

---
+++ Podfile
@@ -0,0 +1,13 @@
+xcodeproj 'SFObjectMapping/SFObjectMapping.xcodeproj'
+
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'SFObjectMapping' do
+
+end
+
+target 'SFObjectMappingTests', :exclusive => true do
+  pod 'Kiwi'
+end
+
---
+++ SFOObjcExample/SFOObjcExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SFOObjcExample
+//
+//  Created by Paul Taykalo on 4/20/15.
+//  Copyright (c) 2015 Stanfy LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
